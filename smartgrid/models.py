a4435b93f9ea9f713f370b160b159475c18db78b
Tijs-B <tijs.bergmans@student.kuleuven.be>
2015-10-15 16:19:39 +0200

    Fixed models

--- smartgrid/models.py
+++ smartgrid/models.py
@@ -20,11 +20,7 @@ class Room(models.Model):
 
 
 class Appliance(models.Model):
-<<<<<<< HEAD
-    room = models.ForeignKey(Room)
-    priority = models.IntegerField()
 
-=======
     room = models.ForeignKey("Room")
     priority = models.IntegerField(default=0, choices=(
         (0, 'Low'),
@@ -33,7 +29,6 @@ class Appliance(models.Model):
         (3, 'Very High'))
     )
 
->>>>>>> origin/master
 
 ### Sensor ###
 class Sensor(models.Model):
