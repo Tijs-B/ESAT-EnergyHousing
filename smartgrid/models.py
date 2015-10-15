1954cecd8b300d9518b2e183623f48b92b6a20e9
VincentAel <vincent.aelbrecht@gmail.com>
2015-10-15 15:30:20 +0200

    Heat load ,Variable power

--- smartgrid/models.py
+++ smartgrid/models.py
@@ -17,7 +17,23 @@ class Room(models.Model):
 class Appliance(models.Model):
     room = models.ForeignKey(Room)
     priority = models.IntegerField()
-
+class Heatload(models.Model):
+     appliance = models.ForeignKey(Appliance)
+class Variablepower(models.Model):
+    heatload = models.ForeignKey(Heatload)
+    VeryCold = 'T<-10'
+    Cold = 'T<0'
+    Normal = '0<T<20'
+    Hot = 'T>20'
+    temppossibilities = ((VeryCold,'T<-10'),(Cold, 'T<0'),( Normal, '0<T<20'),(Hot,'T>20'))
+    temprange = models.CharField(max_length=10,choices=temppossibilities,default=Cold)
+    VeryLow = '1-50'
+    Low = '50-100'
+    Medium = '100-200'
+    High = '200-500'
+    VeryHigh = '500-1000'
+    powerpossibilities = ((VeryLow,'1-50'),(Low,'50-100'),(Medium,'100-200'),(High,'200-500'),(VeryHigh,'500-100'))
+    powerrange = models.CharField(max_length=10,choices=powerpossibilities,default=Medium)
 
 ### Sensor ###
 class Sensor(models.Model):
