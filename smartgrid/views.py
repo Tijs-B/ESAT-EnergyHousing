58c01c15f2751f55d442d022fc8b38e83ca9b2b9
Tijs-B <tijs.bergmans@student.kuleuven.be>
2015-10-15 17:33:19 +0200

    Added testpage view

--- smartgrid/views.py
+++ smartgrid/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.template import RequestContext, loader
+
+from .models import *
 
 # Create your views here.
+
+def testpage(request):
+    template = loader.get_template('smartgrid/testpage.html')
+    return HttpResponse(template.render())
