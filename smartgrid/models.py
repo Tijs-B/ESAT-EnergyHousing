ec5bb250f495f60313b08d96bc152bc31aa9e7ec
Wouter Follon <wouter.follon00@gmail.com>
2015-10-15 15:12:02 +0200

    Poging tot eerste 2 appliances

--- smartgrid/models.py
+++ smartgrid/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from numpy import *
 
 # Create your models here.
+
 
 class Neighbourhood(models.Model):
     energy_price = models.FloatField()
@@ -17,3 +19,26 @@ class Room(models.Model):
 class Appliance(models.Model):
     room = models.ForeignKey(Room)
     priority = models.IntegerField()
+
+    class FixedDemand(models.Model):
+        consumption = models.FloatField()
+        active = models.BooleanField()
+
+    class ShiftingLoadCycle(models.Model):
+        due = models.DateTimeField('Tot: ')
+
+        def consumption_profile(self, length, power):
+            """
+            Returns a 2*len(length) matrix. In the first column you find the time
+            in the second column, you find the power at that time.
+            """
+            profile = zeros(shape=(len(length), 2))
+            index = 0
+            for t in length:
+                profile[index][0] = length[t]
+                profile[index][1] = power[t]
+                index += 1
+            return profile
+
+
+
