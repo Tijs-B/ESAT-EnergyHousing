319e7669bf645320713b3e1f028252e67f152199
Tijs-B <tijs.bergmans@student.kuleuven.be>
2015-10-15 14:44:27 +0200

    Added classes

--- smartgrid/models.py
+++ smartgrid/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Neighbourhood(models.Model):
+    energy_price = models.FloatField()
+
+
+class House(models.Model):
+    neighbourhood = models.ForeignKey(Neighbourhood)
+
+
+class Room(models.Model):
+    house = models.ForeignKey(House)
+
+
+class Appliance(models.Model):
+    room = models.ForeignKey(Room)
+    priority = models.IntegerField()
