91262a4aa7255e76890bae53d0983606b23c0ae6
Wouter Follon <wouter.follon00@gmail.com>
2015-11-09 16:53:13 +0100

    Voorbereiding projectverdeling + bases

--- smartgrid/views.py
+++ smartgrid/views.py
@@ -50,6 +50,7 @@ def auth_view(request):
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
     user = auth.authenticate(username=username, password=password)
+    print user
 
     if user is not None:
         auth.login(request, user)
