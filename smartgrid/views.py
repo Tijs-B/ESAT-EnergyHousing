bac8da76f32eddf40b3bbf4115d27cd64a1876a1
Wouter Follon <wouter.follon00@gmail.com>
2015-12-03 15:51:16 +0100

    enkel nog error bij delete appliance

--- smartgrid/views.py
+++ smartgrid/views.py
@@ -172,7 +172,7 @@ def add_appliance(request, room_id):
     unwanted_appliances = list(chain(HeatLoadInvariablePower.objects.filter(room_id=room_id),
                                      HeatLoadVariablePower.objects.filter(room_id=room_id),
                                      ShiftingLoadCycle.objects.filter(room_id=room_id)))
-    wanted_appliances = all_appliances - unwanted_appliances
+    wanted_appliances = [x for x in all_appliances if x not in unwanted_appliances]
     return render(request, 'smartgrid/post_login/appliances/add_appliance.html',
                   {'room': room, 'appliances': wanted_appliances})
 
@@ -180,16 +180,22 @@ def add_appliance(request, room_id):
 def add(request, room_id):
     r = get_object_or_404(Room, pk=room_id)
     error_message = "Gelieve een apparaat te kiezen."
-    appliances = list(chain(HeatLoadInvariablePower.objects.all(),
-                            HeatLoadVariablePower.objects.all(),
-                            ShiftingLoadCycle.objects.all()))
+
+    all_appliances = list(chain(HeatLoadInvariablePower.objects.all(),
+                                HeatLoadVariablePower.objects.all(),
+                                ShiftingLoadCycle.objects.all()))
+    unwanted_appliances = list(chain(HeatLoadInvariablePower.objects.filter(room_id=room_id),
+                                     HeatLoadVariablePower.objects.filter(room_id=room_id),
+                                     ShiftingLoadCycle.objects.filter(room_id=room_id)))
+    wanted_appliances = [x for x in all_appliances if x not in unwanted_appliances]
+
     try:
         selected_choice = request.POST['appliance']
     except KeyError:
         return render(request, 'smartgrid/post_login/appliances/add_appliance.html', {
             'room': r,
             'error_message': "Gelieve een apparaat te kiezen",
-            'appliances': appliances})
+            'appliances': wanted_appliances})
 
     appliance = list(chain(ShiftingLoadCycle.objects.all().filter(appliance_name=selected_choice),
                            HeatLoadInvariablePower.objects.all().filter(appliance_name=selected_choice),
@@ -211,7 +217,7 @@ def add(request, room_id):
         return render(request, 'smartgrid/post_login/appliance/add_appliance.html', {
             'room': r,
             'error_message': "Het lijkt erop dat er iets is misgegaan, probeer opnieuw a.u.b.",
-            'appliances': appliances})
+            'appliances': wanted_appliances})
 
 
 def delete_appliance(request, room_id):
