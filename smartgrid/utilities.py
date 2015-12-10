07310d6270d822e4c7401fd87c158443f9637254
Tijs-B <tijs.bergmans@student.kuleuven.be>
2015-12-10 16:39:21 +0100

    send to pi indices

--- smartgrid/utilities.py
+++ smartgrid/utilities.py
@@ -10,7 +10,15 @@ def send_to_pi(time):
     scenario = Scenario.objects.all()[0]
     onoffinfo = OnOffInfo.objects.filter(time=time)
     # om vaste id's te geven: bv: {diepvries_huis_A: 1, diepvries_huis_B: 2,...}
-    fixed_appliance_dictionary = {}
+    appliance_dictionary = {'koelkast 0': 2,
+                            'diepvries 0': 4,
+                            'afwasmachine 0': 10,
+                            'auto_groen 0': 16,
+                            'droogkast 0': 17,
+                            'licht 0': 21,
+                            'ventilator 0': 22,
+                            'fornuis 0': 24,
+                            'wasmachine 0': 26}
     list_to_send = []
     for onoff in onoffinfo:
         if onoff.house.neighborhood_name == scenario.current_neighborhood:
@@ -18,8 +26,9 @@ def send_to_pi(time):
             house = onoff.house.house_name
             status = onoff.info
             appliance_name = onoff.appliance_name
-            # appliance_id = fixed_appliance_list[appliance_name]
-            list_to_send += [[house, status, appliance_name]]
+            appliance_id = appliance_dictionary[appliance_name]
+            list_to_send += [[house, status, appliance_id]]
+
     verstuur(list_to_send)
 
 
